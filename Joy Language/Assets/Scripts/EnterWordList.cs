f00b8de7702bf9a01cd2928b259d8d300d74ca5a
Ken Young <kyh649782182@gmail.com>
2019-05-09 17:03:53 -0400

    word reciting first two pages Ui template

---
+++ Joy Language/Assets/Scripts/EnterWordList.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnterWordList : MonoBehaviour
+{
+    public int DiffcultyLevel;
+    public GameObject LevelChoice;
+    public GameObject UnitChoice;
+
+    public void EnterLevel()
+    {
+        LoadData();
+    }
+
+    private void LoadData()
+    {
+        //Load data here, after it finishes call back to show selected level
+
+        LevelChoice.SetActive(false);
+        UnitChoice.SetActive(true);
+
+    }
+}
