603f947e2d1992bca7e998e8e4196396282b61da
colorzzr <colorzzr@gmail.com>
2019-05-12 20:53:27 -0400

    adding the new library json.net for parse the user info in json, still under the test

---
+++ Joy Language/Assets/Scripts/UserLogin.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
+using Newtonsoft.Json;
+
+//probably using the following json format
+//{
+//    'userName':{
+//        'password':'pwd',
+//        'email':'mail'
+//    },
+//    'userName2':{
+//        'password':'pwd',
+//        'email':'mail'
+//    },
+//}
+
+
+public class UserInfo {
+    public string userName;
+    public string password;
+    public string email;
+}
+
+public class UserLogin : MonoBehaviour
+{
+    [SerializeField] Button login;
+
+    UserInfo myObject = new UserInfo();
+    UserInfo myObject2 = new UserInfo();
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //print("test..");
+        //myObject.userName = "test";
+        //myObject.password = "test";
+        //myObject.email = "test@mail";
+        //string json = JsonUtility.ToJson(myObject);
+        //print(json);
+        //File.WriteAllText(@"./Assets/Scripts/userInfo.json", json);
+        string input = File.ReadAllText(@"./Assets/Scripts/userInfo.json");
+        //print(input);
+        //myObject2 =  JsonUtility.FromJson<UserInfo>(input);
+        //print(myObject2.userName);
+
+        JsonTextReader reader = new JsonTextReader(new StringReader(input));
+        while (reader.Read()) {
+            if (reader.Value != null) {
+                print("key: " + reader.TokenType + " Value: " + reader.Value);
+            }
+            else {
+                print("Key: " + reader.TokenType);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
