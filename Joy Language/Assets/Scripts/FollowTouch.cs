5b2286eee87e8adea094009d279893125514c294
Ken Young <kyh649782182@gmail.com>
2019-05-16 13:18:39 -0400

    shark game draft

---
+++ Joy Language/Assets/Scripts/FollowTouch.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTouch : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // every frame
+       
+        // if left-mouse-button is being held OR there is at least one touch
+        if (Input.GetMouseButton(0) || Input.touchCount > 0)
+        {
+            // get mouse position in screen space
+            // (if touch, gets average of all touches)
+            Vector3 screenPos = Input.mousePosition;
+            // set a distance from the camera
+            //screenPos.z = 500.0f;
+            // convert mouse position to world space
+            Vector3 worldPos = Camera.main.ScreenToWorldPoint(screenPos);
+
+            // get current position of this GameObject
+            Vector3 newPos = transform.position;
+            // set x position to mouse world-space x position
+            newPos.x = worldPos.x;
+            newPos.y = worldPos.y;
+
+            //get orthographic camera height and width
+            float height = 2f * Camera.main.orthographicSize;
+            float width = height * Camera.main.aspect;
+
+            if (newPos.y < -130)
+            {
+                newPos.y = -130;
+            }
+            if (newPos.y > 80)
+            {
+                newPos.y = 80;
+            }
+            if (newPos.x < -width/2 + 0.5f)
+            {
+                newPos.x = -width / 2 + 0.5f;
+            }
+            if (newPos.x > width/2 - 0.5f)
+            {
+                newPos.x = width / 2 - 0.5f;
+            }
+            // apply new position
+            transform.position = newPos;
+        }
+        
+    }
+}
---
+++ Joy Language/Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public TextMeshProUGUI Question;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Question.text = "This bed sheet has multiple _____";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
