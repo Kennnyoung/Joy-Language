c05747f4c93795836a0ee8f6b296141c34bc1bbc
tonycl.wu@mail.utoronto.ca <tonycl.wu@mail.utoronto.ca>
2019-05-16 15:50:03 -0400

    slow-motion

---
+++ Joy Language/Assets/Scripts/TimeManager/TimeManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+
+    public float slowdownFactor = 0.05f;
+
+    public void SlowDown()
+    {
+        Time.timeScale = slowdownFactor;
+    }
+
+    public void SpeedUp()
+    {
+        Time.timeScale = 1;
+    }
+}
