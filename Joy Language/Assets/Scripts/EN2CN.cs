2ac10534fcd0f79f3d99541027a25853af09611b
colorzzr <colorzzr@gmail.com>
2019-05-18 13:07:46 -0400

    add the dynamic vocal pick

--- Joy Language/Assets/Scripts/EN2CN.cs
+++ Joy Language/Assets/Scripts/EN2CN.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
+using System.IO;
+using Newtonsoft.Json;
+using System.Text;
 
 public class EN2CN : MonoBehaviour
 {
@@ -10,6 +12,7 @@ public class EN2CN : MonoBehaviour
     [SerializeField] Button choice2;
     [SerializeField] Button choice3;
     [SerializeField] Button choice4;
+    [SerializeField] Text englishShow;
     [SerializeField] Text checkAns;
 
     string correct;
@@ -17,15 +20,38 @@ public class EN2CN : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
+        //string input = File.ReadAllText("./Assets/Scripts/Vocabulary/word_test.json", Encoding.GetEncoding("gb2312"));
+        //List<Vocabulary> vLst = JsonConvert.DeserializeObject<List<Vocabulary>>(input);
+        //foreach (Vocabulary v in vLst){
+        //    v.printV();
+        //}
+
+        VocabularySheet test = new VocabularySheet("./Assets/Scripts/Vocabulary/word_test.json");
+        
+
+
+
         //VocabularySheet test = new VocabularySheet("./Assets/Scripts/Vocalbulary/word.json");
-        //print(test.GetVList('E'));
+        List<Vocabulary> list = test.GetVList('M');
+        for(int i = 0; i < list.Count; i++) {
+           list[i].printV();
+        }
+
         choice1.onClick.AddListener(delegate { userPick(choice1); });
         choice2.onClick.AddListener(delegate { userPick(choice2); });
         choice3.onClick.AddListener(delegate { userPick(choice3); });
         choice4.onClick.AddListener(delegate { userPick(choice4); });
 
         // i will get the correct CN here
-        correct = choice1.GetComponentInChildren<Text>().text;
+        correct = list[0].Meaning;
+        // set the choice
+        choice1.GetComponentInChildren<Text>().text = list[0].Meaning;
+        choice2.GetComponentInChildren<Text>().text = list[1].Meaning;
+        choice3.GetComponentInChildren<Text>().text = list[2].Meaning;
+        choice4.GetComponentInChildren<Text>().text = list[3].Meaning;
+        // set the EN
+        englishShow.text = list[0].Spelling;
     }
 
     void userPick(Button clicked) {
@@ -40,6 +66,26 @@ public class EN2CN : MonoBehaviour
             checkAns.text = "False";
         }
         Invoke("resetCheckAns", 2);
+    }
+
+    void printAll(VocabularySheet test) {
+        print("------Easy------");
+        foreach (var v in test.EasySheet) {
+            print(v.Key);
+            print(v.Value);
+        }
+
+        print("------Medium------");
+        foreach (var v in test.MediumSheet) {
+            print(v.Key);
+            print(v.Value);
+        }
+
+        print("------Hard------");
+        foreach (var v in test.HardSheet) {
+            print(v.Key);
+            print(v.Value);
+        }
     }
 
     // to reset the checkAns text box
