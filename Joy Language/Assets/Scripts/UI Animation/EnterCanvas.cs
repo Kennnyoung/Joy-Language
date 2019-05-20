6cb0ec6f85505d91561a051af57734aca42a6c2e
Mason-Liu <liumo@sheridancollege.ca>
2019-05-20 14:11:21 -0400

    Change text color and movement ease type

--- Joy Language/Assets/Scripts/UI Animation/EnterCanvas.cs
+++ Joy Language/Assets/Scripts/UI Animation/EnterCanvas.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class EnterCanvas : MonoBehaviour
 {
+    public float duration;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +20,6 @@ public class EnterCanvas : MonoBehaviour
 
     void SetAim(Vector3 tempAim)
     {
-        iTween.MoveTo(gameObject, tempAim,1f);
+        iTween.MoveTo(gameObject, iTween.Hash("position", tempAim, "time", duration, "easetype", "easeInOutBack"));
     }
 }
