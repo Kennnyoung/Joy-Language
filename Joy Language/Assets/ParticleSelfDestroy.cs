1f784cb7d71a9985d5a457c0241d861c599d05d1
Mason-Liu <liumo@sheridancollege.ca>
2019-06-03 15:39:50 -0400

    Add option vanish particle effect

---
+++ Joy Language/Assets/ParticleSelfDestroy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleSelfDestroy : MonoBehaviour
+{
+    ParticleSystem ps;
+    void Awake()
+    {
+        ps = GetComponent<ParticleSystem>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SelfDestroy());
+    }
+
+    IEnumerator SelfDestroy()
+    {
+        float lifeTime = ps.main.duration;
+        yield return new WaitForSeconds(lifeTime);
+        Destroy(gameObject);
+    }
+}
