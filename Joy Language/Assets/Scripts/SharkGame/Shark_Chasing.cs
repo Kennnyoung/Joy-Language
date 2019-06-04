9f9bafdd8a5365264e499e58926c9b6549347435
Mason-Liu <liumo@sheridancollege.ca>
2019-06-04 16:08:13 -0400

    Add knock back behavior to shark

--- Joy Language/Assets/Scripts/SharkGame/Shark_Chasing.cs
+++ Joy Language/Assets/Scripts/SharkGame/Shark_Chasing.cs
@@ -4,17 +4,39 @@ using UnityEngine;
 
 public class Shark_Chasing : MonoBehaviour
 {
-    public float verticalOffset;
+    [SerializeField] float verticalOffset;
+    [SerializeField] float chasingSpeed;
     Vector3 aim;
-    
+
+    [SerializeField] float knockBackDelay;
+    [SerializeField] float knockBackDistance;
+    bool knockBack;
+    Coroutine timer;
     void SetAim(Vector3 tempAim)
     {
         aim = tempAim;
     }
 
+    private void OnParticleCollision(GameObject other)
+    {
+        transform.position += Vector3.down * knockBackDistance;
+        if (timer != null)
+        {
+            StopCoroutine(timer);
+        }
+        knockBack = true;
+        timer = StartCoroutine(BeatBack());
+    }
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector2.Lerp(transform.position, new Vector2(aim.x, aim.y - verticalOffset), 0.05f * Time.deltaTime);
+        if(!knockBack)
+        transform.position = Vector2.Lerp(transform.position, new Vector2(aim.x, aim.y - verticalOffset), chasingSpeed * Time.deltaTime);
+    }
+
+    IEnumerator BeatBack()
+    {
+        yield return new WaitForSeconds(knockBackDelay);
+        knockBack = false;
     }
 }
