6932175b7e3868e9a5768cbbdf59e4d7c9a5e05f
Mason-Liu <liumo@sheridancollege.ca>
2019-07-04 18:50:01 -0400

    Add a card prefab for collection system

---
+++ Joy Language/Assets/Scripts/Learning Partner/Fragments.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fragments : MonoBehaviour
+{
+    public int amount;
+    [Range(0, 10)] public int currentAmount;
+    public Image image;
+    public Image background;
+    public Button btn;
+    public bool isCompleted;
+
+    void Start()
+    {
+        image.type = Image.Type.Filled;
+        Display();
+    }
+
+    void Display()
+    {
+        isCompleted = currentAmount == amount ? true : false;
+
+        if (!isCompleted)
+        {
+            image.fillAmount = (float)currentAmount / amount;
+        }
+        else
+        {
+            image.fillAmount = 1f;
+            background.gameObject.SetActive(false);
+        }
+    }
+
+    //void Update()
+    //{
+    //    image.fillAmount = (float)currentAmount / amount; 
+    //}
+}
