cd9db70bca4df6880d0ba20bdcf2ffad6e56e395
Mason-Liu <liumo@sheridancollege.ca>
2019-07-08 14:10:37 -0400

    Album dropdown menu

---
+++ Joy Language/Assets/Scripts/Learning Partner/Dropdown.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dropdown : MonoBehaviour
+{
+    public GameObject btnObj;
+    public GameObject menu;
+    public Sprite expan;
+    public Sprite back;
+    Button btn;
+    bool isshow = false;
+    // Use this for initialization
+    void Start()
+    {
+        menu.SetActive(isshow);
+        btn = btnObj.GetComponent<Button>();
+        btn.onClick.AddListener(delegate ()
+        {
+            isshow = !isshow;
+            menu.SetActive(isshow);
+            if (isshow)
+            {
+                btn.GetComponent<Image>().sprite = expan;
+            }
+            else
+            {
+                btn.GetComponent<Image>().sprite = back;
+            }
+        });
+    }
+}
